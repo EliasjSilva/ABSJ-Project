1c5cd37006e318ffa756051aa5e2ca5678337f45
IagoSiilva <iagomito22@gmail.com>
2023-12-18 16:19:22 -0300

    Primeiros ajustes - Forms e Tabelas

--- ABSJ/forms.py
+++ ABSJ/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 from . import models as m
+from .models import Contribuidor
+from .models import Categoria
+from .models import Produto
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
-        model = m.Produto
+        model = Produto
         fields = "__all__"
         widgets = {
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'contribuidor': forms.Select(attrs={'class': 'form-control'}),
+            'produto': forms.TextInput(attrs={'class': 'form-control'}),
+            'qtd': forms.NumberInput(attrs={'class': 'form-control'}),
             'validade': forms.DateInput(
                 format=('%d/%m/%Y'),
                 attrs={
+                    'class': 'form-control',
                     'placeholder': 'Select a date',
                     'type': 'date',
                 }
@@ -17,19 +25,26 @@ class ProdutoForm(forms.ModelForm):
 
 class CategoryForm(forms.ModelForm):
     class Meta:
-        model = m.Categoria
+        model = Categoria
         fields = "__all__"
+        widgets = {
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class ContribuidorForm(forms.ModelForm):
     class Meta:
-        model = m.Contribuidor
+        model = Contribuidor
         fields = "__all__"
         widgets = {
+            'contribuidor': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipocontribuidor': forms.TextInput(attrs={'class': 'form-control'}),
             'tempo': forms.DateInput(
                 format=('%d/%m/%Y'),
                 attrs={
+                    'class': 'form-control',
                     'placeholder': 'Select a date',
                     'type': 'date',
                 }
             ),
+            'observacoes': forms.Textarea(attrs={'class': 'form-control'}),
         }
