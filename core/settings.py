d4b1a20980bac7018f00999ca46dd2ce41d4777a
= <=>
2024-01-07 15:44:13 -0300

    Okay!

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^$i*#x=#gq*tp9x^65(njom+_q+5bzgz9u8fi29)#ar-h9xy_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
