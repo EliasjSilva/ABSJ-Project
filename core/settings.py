929e6efccbe91020a797ae1749f9f0523d586a74
EliasjSilva <eliasj.silva.pb@gmail.com>
2025-04-06 18:47:15 -0300

    Deploy inicial na Render

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -124,8 +126,9 @@ if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
---
+++ staticfiles/javascripts/js.5317e760a6a8.js
@@ -0,0 +1,37 @@
+  // Atualiza a data a cada segundo
+  function atualizarData() {
+    var now = new Date();
+    var formattedDate = now.toLocaleString(); // ou qualquer formato desejado
+
+    // Atualiza o conteúdo HTML com a nova data
+    $('#data-atual').text(formattedDate);
+  }
+
+  // Chama a função inicialmente e a cada segundo
+  $(document).ready(function() {
+    atualizarData();
+    setInterval(atualizarData, 1000); // Atualiza a cada segundo
+  });
+
+// POP UP INFO
+function togglePopup(tipo) {
+  if (tipo == 'vencido') {
+    $('.message_vencido').toggle(50);
+  } 
+  else if (tipo == 'prazo') {
+    $('.message_prazo').toggle(50);
+  } 
+  else if (tipo == 'categoria') {
+    $('.delete').toggle(50);
+  } 
+  else if (tipo == 'contribuidor') {
+    $('.delete').toggle(50);
+  } 
+  else if (tipo == 'produto') {
+    $('.delete').toggle(50);
+  } 
+  else {
+    // Se o tipo não for 'vencido' ou 'prazo', você pode ocultar ambos
+    $('.message_vencido, .message_prazo').hide();
+  }
+}
